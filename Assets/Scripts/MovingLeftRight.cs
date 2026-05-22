7a4d6395b7c90c0843afd420e986d6a34d0a6981
ZAA18 <Zubuhle01@gmail.com>
2026-05-22 18:55:41 +0200

    Using different script on the duplicated Moving block
    Works

--- Assets/Scripts/MovingLeftRight.cs
+++ Assets/Scripts/MovingLeftRight.cs
@@ -1,5 +1,43 @@
 using UnityEngine;
 
+public class MovingObstacle : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveDistance = 5f;
+
+    public float moveSpeed = 2f;
+
+    public bool moveHorizontally = true;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Creates:
+        // Middle -> Right -> Middle -> Left -> Middle
+
+        float movement = Mathf.Sin(Time.time * moveSpeed) * moveDistance;
+
+        if (moveHorizontally)
+        {
+            transform.position = startPosition + new Vector3(movement, 0, 0);
+        }
+        else
+        {
+            transform.position = startPosition + new Vector3(0, movement, 0);
+        }
+    }
+}
+
+
+
+/*using UnityEngine;
+
 public class MovingLeftRight : MonoBehaviour
 {
     public float moveDistance = 5f;
@@ -39,3 +77,4 @@ public class MovingLeftRight : MonoBehaviour
         rb.MovePosition(targetPosition);
     }
 }
+*/
